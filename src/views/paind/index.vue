e6994464080cd4dd627e47cf2cf319bd4f42ec25
Bekzat <utebalievbekzat@gmail.com>
2024-02-28 23:44:29 +0600

    second page

--- src/views/paind/index.vue
+++ src/views/paind/index.vue
@@ -1,29 +1,183 @@
 <template>
-
+  <div class="navbar_first">
   <div class="navbar">
     <div class="main_navbar">
-      <div class="log_name">
-        <div class="log_img"><img height="43px" src="@/assets/images/logo.png"></div>
-        <div class="name_log">EmpowerED</div>
+      <div class="left_side_navbar">
+        <div class="log_name">
+          <div class="log_img"><img height="32px" src="@/assets/images/logo.png"></div>
+          <div class="name_log">EmpowerED</div>
+        </div>
+        <div class="second">
+          <div>Browse</div>
+          <div class="down_img"><img width="13.11px" src="@/assets/images/down.png"></div>
+        </div>
       </div>
-      <div class="second">
-        <div>Browse</div>
-        <div class="down_img"><img width="13.11px" src="@/assets/images/down.png"></div>
-      </div>
-      <div class="search">
+      <div class="search_navbar">
         <input type="search" placeholder="Search for course">
         <img class="icon_search" height="21px" src="@/assets/images/Search.png">
 
       </div>
-      <div class="basket_img"><img height="45.62px" src="@/assets/images/basket.png"></div>
-      <div class="basket_img"><img height="45.62px" src="@/assets/images/notifications.png"></div>
-      <div class="basket_img"><img height="45.62px" src="@/assets/images/avatar_icone.png"></div>
-
+      <div class="icons">
+        <div class="basket_img"><img height="24px" src="@/assets/images/basket.png"></div>
+        <div class="basket_img"><img height="24px" src="@/assets/images/notifications.png"></div>
+        <div class="basket_img"><img height="36px" src="@/assets/images/avatar_icone.png"></div>
+      </div>
     </div>
   </div>
+  </div>
   <div class="main">
+    <div class="main_head">
+  <div class="left_side">
+    <iframe width="707px" height="372px" src="https://www.youtube.com/embed/WiWuy6yIx40?si=D4X4R7sQsrlHx3OI"  allow=" autoplay;  " allowfullscreen></iframe>
+    <div class="text_under_video">VUE JS SCRATCH COURSE</div>
+  </div>
+    <div class="right_side_main">
+
+      <div class="head_list">
+        <div class="first_list">
+          Chapter 1: Course Overview
+        </div>
+        <div class="head_second_list">
+          <div class="text_second_list"> 1/12 Videos</div>
+          <img src="@/assets/images/Ellipse.png" height="3px">
+          <div class="blue_text">28m</div>
+          <div class="right_side_list"></div>
+
+        </div>
+
+      </div>
+      <div class="second_list"></div>
+      <div class="list_item"></div>
+      <div class="list_item"></div>
+      <div class="list_item"></div>
+      <div class="list_item"></div>
+      <div class="list_item"></div>
+      <div class="list_item"></div>
+      <div class="list_item"></div>
+      <div class="list_item"></div>
+    </div>
+    </div>
+    <div class="head_under">
+      <div class="right_yellow">
+      <img src="@/assets/images/Square.png" height="55px">
+      <div class="yellow_text">
+        Kitani Studio
+        <div>Design Studio</div>
+      </div>
+      </div>
+      <div class="left_yellow">
+      <div class="user_icon">
+        <img src="@/assets/images/supervised.png" height="49px">
+        2.3k
+      </div>
+      <div class="chat_icon">
+        <img src="@/assets/images/rate.png" height="49px">
+        1.4k
+      </div>
+      </div>
+    </div>
+    <div class="main_page">
+      <div class="first_main_text">
+      <div class="first_main">
+    <div class="first_text">
+      About Course
+    </div>
+      <div class="about">Vue (pronounced /vjuː/, like view) is a progressive framework for building user interfaces. Unlike other monolithic frameworks, Vue is designed from the ground up to be incrementally adoptable. The core library is focused on the view layer only, and is easy to pick up and integrate with other libraries or existing projects. On the other hand, Vue is also perfectly capable of powering sophisticated Single-Page Applications when used in combination with modern tooling and supporting libraries.</div>
+    </div>
+        <div class="second_main_text">
+          Review
+          <div class="first_item">
+                <img src="@/assets/images/ava.png" height="33px">
+                <div style="margin-left: 16px">
+                  <div class="Blue_text">Leonardo  Da Vinci</div>
+                  <div class="center_text">Today</div>
+                  <div class="end_text">Loved the course. I've learned some very subtle tecniques, </div>
+                </div>
+          </div>
+          <div class="second_item">
+            <img src="@/assets/images/second_item.png" height="33px">
+            <div style="margin-left: 16px">
+              <div class="Blue_text">Titania  S</div>
+              <div class="center_text">Today</div>
+              <div class="end_text">I loved the course, it had been a long time since I had experimented with watercolors and now I will do it more often thanks to Kitani Studio </div>
+            </div>
+          </div>
+          <div class="third_item">
+            <img src="@/assets/images/third_item.png" height="33px">
+            <div style="margin-left: 16px">
+              <div class="Blue_text">Zhirkov</div>
+              <div class="center_text">Today</div>
+              <div class="end_text">Yes. I just emphasize that the use of Photoshop, for non-users, becomes difficult to follow. What requires a course to master it. Safe and very didactic teacher.</div>
+            </div>
+          </div>
+          <div class="fourth_item">
+            <img src="@/assets/images/fourth_item.png" height="33px">
+            <div style="margin-left: 16px">
+              <div class="Blue_text">Titania  S</div>
+              <div class="center_text">Today</div>
+              <div class="end_text">I haven't finished the course yet, as I would like to have some feedback from the teacher, about the comments I shared on the forum 3 months ago, and I still haven't had any answer.
+                I think the course is well structured, however the explanations and videos are very quick for beginners. However, it is good to go practicing.</div>
+            </div>
+          </div>
 
 
+        </div>
+      </div>
+      
+      <img src="@/assets/images/Group.png" height="697px">
+    </div>
+    <div class="search">
+      <div style="width: 800px; height: 53px; display: flex; align-items: center; justify-content: space-between; margin: 0 auto">
+      <div class="search_text">
+        Join and get amazing discount
+        <p>With our responsive themes and mobile and desktop apps</p>
+      </div>
+      <div class="input">
+        <input type="search">
+        <img src="@/assets/images/search_last.png" height="15px">
+      </div>
+      <button type="button">Subscribe</button>
+      </div>
+    </div>
+  </div>
+
+    <div style="background-color: black">
+    <div class="footer">
+      <div class="footer_main">
+        <div class="right_side">
+          <div class="log_img"><img height="43px" src="@/assets/images/logo.png"></div>
+          <div class="name_log_last">EmpowerED</div>
+        </div>
+        <div class="second_footer">
+          <div>Web Programming</div>
+          <div>Mobile Programming</div>
+          <div>Java Beginner</div>
+          <div>PHP Beginner</div>
+        </div>
+        <div class="third_footer">
+          <div>Adobe Illustrator</div>
+          <div>Adobe Photoshop</div>
+          <div>Design Logo</div>
+
+        </div>
+        <div class="fourth_footer">
+          <div>Writing Course</div>
+          <div>Photography</div>
+          <div>Video Making</div>
+        </div>
+      </div>
+      <hr style="width: 1691px; border: 0.5px solid rgba(249,249,249,0.9)">
+      <div class="last_text">
+        <div class="last_right_text">Copyright © EmpowerED 2024. All Rights Reserved</div>
+        <div class="last_left_text">
+          <img src="@/assets/images/Twitter.png" height="25px">
+          <img src="@/assets/images/Instagram.png" height="25px">
+          <img src="@/assets/images/Facebook.png" height="25px">
+        </div>
+      </div>
+
+
+  </div>
   </div>
 
 
@@ -49,6 +203,204 @@ export default {
 </script>
 
 <style scoped>
+.last_left_text{
+  display: flex;
+  justify-content: space-between;
+  width: 150px;
+
+}
+.last_right_text{
+  font-size: 16px;
+  color: rgba(249, 249, 249, 0.6);
+  font-family: "Ubuntu-regular", sytem ui;
+}
+.last_text{
+  display: flex;
+  width: 1693px;
+  margin: 0 auto;
+  justify-content: space-between;
+}
+.footer_main{
+  width: 1408.33px;
+  height: 138.26px;
+  display: flex;
+  justify-content: space-between;
+  padding-top: 40px;
+  padding-left: 105px;
+
+}
+.fourth_footer{
+  color: #FFFFFF;
+  font-size: 16px;
+  font-family: "Ubuntu-regular", sytem ui;
+
+}
+.third_footer{
+  color: #FFFFFF;
+  font-size: 16px;
+  font-family: "Ubuntu-regular", sytem ui;
+}
+
+.second_footer{
+  color: #FFFFFF;
+  font-size: 16px;
+  font-family: "Ubuntu-regular", sytem ui;
+  width: 201.33px;
+  height: 136.81px;
+}
+.right_side{
+  display: flex;
+  align-items: center;
+  height: 43px;
+  width: 152.8px;
+  justify-content: space-between;
+}
+.search{
+  width: 904px;
+  height: 139px;
+  border-radius: 12px;
+  background-color: #1365E1;
+  margin-bottom: 142px;
+}
+.second_main_text{
+  font-size: 16px;
+  font-family: "Ubuntu-bold", sytem ui;
+
+}
+.fourth_item{
+  width: 469px;
+  height: 130px;
+  display: flex;
+  margin-top: 17px;
+}
+.third_item{
+  height: 94px;
+  width: 518px;
+  display: flex;
+  margin-top: 17px;
+}
+.second_item{
+  display: flex;
+  width: 519px;
+  margin-top: 17px;
+  height: 77px;
+}
+.center_text{
+  font-size: 10px;
+  font-family: "Ubuntu-regular", sytem ui;
+  color: rgba(27, 27, 27, 0.1);
+}
+.end_text{
+  font-size: 14px;
+  color: rgba(27, 27, 27, 0.6);
+  font-family: "Ubuntu-regular", sytem ui;
+}
+.Blue_text{
+  color: #1365E1;
+  font-size: 16px;
+  font-family: "Ubuntu-bold", sytem ui;
+}
+.first_item{
+  display: flex;
+  width: 824px;
+  height: 58px;
+  margin-top: 17px;
+}
+.first_text{
+  font-size: 16px;
+  font-family: "Ubuntu-bold", sytem ui;
+  margin-bottom: 16px;
+  margin-top: 13px;
+}
+.about{
+  font-family: "Ubuntu-regular", sytem ui;
+  font-size: 16px;
+  color: rgba(27, 27, 27, 0.6);
+}
+.first_main{
+  width: 519px;
+  height: 229px;
+}
+.main_page{
+  width: 1759px;
+  height: 736px;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 200px;
+}
+.text_under_video{
+  font-size: 24px;
+  font-family: "Ubuntu-bold", sytem ui;
+  margin-top: 25px;
+}
+.yellow_text{
+  font-size: 20px;
+  color: #F2C94C;
+  font-family: "Ubuntu-bold", sytem ui;
+}
+.yellow_text div{
+  color: rgba(27, 27, 27, 0.6);
+  font-size: 15px;
+  font-family: "Ubuntu-regular", sytem ui;
+}
+.left_yellow{
+  display: flex;
+  width: 270px;
+  justify-content: space-between;
+  font-family: "Ubuntu-regular", sytem ui;
+  font-size: 20px;
+  color: rgba(27, 27, 27, 0.6);
+}
+.right_yellow{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 200px;
+
+}
+.user_icon{
+  width: 104px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+}
+.chat_icon{
+  width: 104px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.head_under{
+  width: 938px;
+  height: 66px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.main_head{
+  display: flex;
+  width: 1430px;
+  height: 448px;
+  justify-content: space-between;
+}
+.right_side_main{
+  width: 363px;
+  height: 448px;
+  background-color: #F4F4F4;
+  border-radius: 12px;
+}
+.navbar_first{
+  width: 1024px;
+  display: flex;
+  justify-content: center;
+}
+.left_side_navbar{
+  width: 222px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 
 .main{
   width: 1780px;
@@ -65,10 +417,10 @@ export default {
 .log_name{
   display: flex;
   align-items: center;
-  height: 43px;
-  width: 151.49px;
+  height: 30px;
+  width: 120px;
   justify-content: space-between;
-  font-size: 17.79px;
+  font-size: 14px;
   font-family: "Ubuntu-bold",sytem ui;
 
 }
@@ -82,24 +434,43 @@ export default {
   font-family: "Ubuntu-bold",sytem ui;
 }
 .navbar{
-  height: 114.06px;
-  width: 1718px;
-  margin: 0 auto;
+  height: 60px;
+  width: 927px;
   margin-top: 23.08px;
-  box-shadow: 0 2px 2px 2px rgba(0, 0, 0, 0.06);
   border-radius: 30px;
   font-family: "Ubuntu-regular",sytem ui;
   font-size: 16px;
   display: flex;
   align-items: center;
   margin-bottom: 28.94px;
+  justify-content: start;
 }
-.search input{
-  width: 678px;
+.search_navbar input{
+  width: 400px;
   height: 37px;
-  border-radius: 12px;
+  border-radius: 3px;
   background-color: rgba(249, 249, 249, 0.9);
   border: none;
+
+}
+.search_navbar{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.second{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 79px;
+  height: 21px;
+}
+.icons{
+  width: 116px;
+  height: 36px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 
