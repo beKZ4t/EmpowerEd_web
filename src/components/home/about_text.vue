2ff2db09452c16d0dc88d752240579e480d9730e
Bekzat <utebalievbekzat@gmail.com>
2024-03-06 17:27:13 +0500

    asa

---
+++ src/components/home/about_text.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="block">
+    <div class="left_side">
+      <h2>About Course</h2>
+      <div class="about">
+        Vue (pronounced /vjuː/, like view) is a progressive framework for building user interfaces. Unlike other monolithic frameworks, Vue is designed from the ground up to be incrementally adoptable. The core library is focused on the view layer only, and is easy to pick up and integrate with other libraries or existing projects. On the other hand, Vue is also perfectly capable of powering sophisticated Single-Page Applications when used in combination with modern tooling and supporting libraries.
+      </div>
+      <div class="reviews">
+        <div class="head_reviews">Review</div>
+        <div class="people">
+          <div class="avatar">
+            <img class="avatar-image" :src="reviewBlock.avatar">
+          </div>
+          <p class="name_person">{{reviewBlock.name}}</p>
+          <p class="date">{{reviewBlock.date}}</p>
+          <p class="people_review">{{reviewBlock.review}}</p>
+
+
+        </div>
+
+      </div>
+
+
+    </div>
+
+
+    <div class="right_side">
+
+      <img src="@/assets/images/Group.png" >
+    </div>
+
+  </div>
+
+</template>
+
+
+<script >
+export default {
+  name: "about_text-component",
+
+}
+</script>
+<style scoped>
+.block{
+  width: 90%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+}
+.left_side{
+  width: 30%;
+}
+h2{
+  font-size: 16px;
+  font-family: "Ubuntu-bold", sytem ui;
+}
+.about{
+  font-family: Ubuntu-regular, system-ui;
+  font-size: 16px;
+  color: rgba(27, 27, 27, 0.6);
+  margin-top: 16px;
+}
+.right_side img{
+  height: 90%;
+}
+</style>
--- src/views/paind_2/index.vue
+++ src/views/paind_2/index.vue
@@ -2,6 +2,7 @@
   <header-component />
   <video_blcok-component />
   <header_text-component />
+  <about_text-component />
 
 
 
@@ -24,16 +25,48 @@ import FooterBlockComponent from "@/components/home/footer_block.vue";
 import LessonExpandableComponent from "@/components/home/lesson_expandable.vue";
 import Video_blcokComponent from "@/components/home/video_block.vue";
 import Header_textComponent from "@/components/home/header_text.vue";
+import About_textComponent from "@/components/home/about_text.vue";
 
 export default {
   name: "index",
   components: {
+    About_textComponent,
     Header_textComponent,
     Video_blcokComponent, LessonExpandableComponent, FooterBlockComponent, HeaderComponent},
+  data() {
+    return {
+      reviewBlock: [
+        {
+          avatar: "http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcRM0gr38i7a0yNb0upDr9HiY0phSLCJzcqLJCfGNHmjYH_2yUpgBpME6o44lbqUtb4wWBEwxYgfy-n85LKmTn0",
+          name: "Leonardo  Da Vinci",
+          date: "Today",
+          review: "Loved the course. I've learned some very subtle tecniques, ",
+        },
+        {
+          avatar: "http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcRM0gr38i7a0yNb0upDr9HiY0phSLCJzcqLJCfGNHmjYH_2yUpgBpME6o44lbqUtb4wWBEwxYgfy-n85LKmTn0",
+          name: "Leonardo  Da Vinci",
+          date: "Today",
+          review: "Loved the course. I've learned some very subtle tecniques, ",
+        },
+        {
+          avatar: "http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcRM0gr38i7a0yNb0upDr9HiY0phSLCJzcqLJCfGNHmjYH_2yUpgBpME6o44lbqUtb4wWBEwxYgfy-n85LKmTn0",
+          name: "Leonardo  Da Vinci",
+          date: "Today",
+          review: "Loved the course. I've learned some very subtle tecniques, ",
+        },
+        {
+          avatar: "http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcRM0gr38i7a0yNb0upDr9HiY0phSLCJzcqLJCfGNHmjYH_2yUpgBpME6o44lbqUtb4wWBEwxYgfy-n85LKmTn0",
+          name: "Leonardo  Da Vinci",
+          date: "Today",
+          review: "Loved the course. I've learned some very subtle tecniques, ",
+        }
 
-  mounted() {
-    window.scrollTo(0, 0);
+      ]
+    }
   }
+
+
+
 
 }
 </script>
