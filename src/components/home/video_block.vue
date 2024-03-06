cf8141b08cab9b07d66447eab8c2f72089c4fa8f
Bekzat <utebalievbekzat@gmail.com>
2024-03-06 15:15:44 +0500

    asa

---
+++ src/components/home/video_block.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="video_block">
+    <div class="left_side">
+      <iframe  src="https://www.youtube.com/embed/WiWuy6yIx40?si=D4X4R7sQsrlHx3OI"  allow=" autoplay;  " allowfullscreen></iframe>
+<!--      <div class="text_under_video">VUE JS SCRATCH COURSE</div>-->
+    </div>
+    <div class="right_side_main">
+      <lesson-expandable-component />
+    </div>
+
+  </div>
+</template>
+<script>
+import LessonExpandableComponent from "@/components/home/lesson_expandable.vue";
+
+export default {
+  name: "video_block-component",
+  components: {LessonExpandableComponent},
+}
+</script>
+
+<style scoped>
+.video_block{
+  width: 90%;
+  margin: 0 auto;
+  margin-top: 90px;
+  display: flex;
+  justify-content: space-between;
+
+}
+iframe{
+  width: 707px;
+  height: 372px;
+
+}
+.text_under_video{
+  font-size: 24px;
+  font-family: "Ubuntu-bold", sytem ui;
+  margin-top: 26px;
+}
+.right_side_main{
+  padding-right: 20%;
+}
+@media screen and (max-width: 1200px) {
+  .video_block{
+
+
+  }
+  iframe{
+    width: 500px;
+  }
+  .right_side_main{
+    margin-left: 5%;
+  }
+}
+@media screen and (max-width: 880px) {
+  .video_block{
+    display: ruby-text;
+
+  }
+}
+</style>
--- src/views/paind_2/index.vue
+++ src/views/paind_2/index.vue
@@ -1,16 +1,8 @@
 <template>
   <header-component />
+  <video_blcok-component />
 
-  <div class="video_block">
-    <div class="left_side">
-      <iframe width="707px" height="372px" src="https://www.youtube.com/embed/WiWuy6yIx40?si=D4X4R7sQsrlHx3OI"  allow=" autoplay;  " allowfullscreen></iframe>
-      <div class="text_under_video">VUE JS SCRATCH COURSE</div>
-    </div>
-    <div class="right_side_main">
-      <lesson-expandable-component />
-    </div>
 
-  </div>
 
 
 
@@ -30,10 +22,11 @@
 import HeaderComponent from "@/components/home/header.vue";
 import FooterBlockComponent from "@/components/home/footer_block.vue";
 import LessonExpandableComponent from "@/components/home/lesson_expandable.vue";
+import Video_blcokComponent from "@/components/home/video_block.vue";
 
 export default {
   name: "index",
-  components: {LessonExpandableComponent, FooterBlockComponent, HeaderComponent},
+  components: {Video_blcokComponent, LessonExpandableComponent, FooterBlockComponent, HeaderComponent},
 
   mounted() {
     window.scrollTo(0, 0);
